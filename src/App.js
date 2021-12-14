8dec366a9b62809a2da7a80acf738728e6270c19
Floyd-88 <ilasaz1988@gmail.com>
2021-12-15 00:00:38 +0300

    css components 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Component/Header';
-import NavBar from './Component/NavBar';
-import MainContent from './Component/MainContent';
+import Header from './Component/Header/Header';
+import NavBar from './Component/NavBar/NavBar';
+import MainContent from './Component/MainContent/MainContent';
 
 
 const App = () => {
